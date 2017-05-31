d9551095056dbe9b6da28bbd78d18032d43ec4b1
celalkucukbulut <celalkucukbulut@gmail.com>
2017-05-31 17:55:59 +0300

    Admin/Home/FileUpload

--- Dilek/Areas/Admin/Controllers/AnasayfaController.cs
+++ Dilek/Areas/Admin/Controllers/AnasayfaController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,17 @@ namespace Dilek.Areas.Admin.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFiles(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Images"), fileName);
+                file.SaveAs(path);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
