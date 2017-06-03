e6919c04e59486be9be353b69627a6189abef41c
celalkucukbulut <celalkucukbulut@gmail.com>
2017-06-03 17:18:26 +0300

    KastamonuFanilası and Hakkımızda done Admin

--- Dilek/Areas/Admin/Controllers/HakkımızdaController.cs
+++ Dilek/Areas/Admin/Controllers/HakkımızdaController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain.Domains;
+using Services.ContentsServices;
+using Services.ImagesServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,53 @@ namespace Dilek.Areas.Admin.Controllers
 {
     public class HakkımızdaController : BaseController
     {
+        public readonly IContentsServices _contentsServices;
+        public HakkımızdaController()
+        {
+        }
+        public HakkımızdaController(
+            IContentsServices contentsServices)
+        {
+            _contentsServices = contentsServices;
+        }
         // GET: Admin/Hakkımızda
         public ActionResult Index()
         {
-            return View();
+            var result = _contentsServices.getAllContentsByDBCode(2);
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateText(int ID, string title, string text, DateTime CreatedDate, int DBCode)
+        {
+            Contents content = new Contents()
+            {
+                CreatedDate = CreatedDate,
+                DBCode = DBCode,
+                ID = ID,
+                Text = text,
+                Title = title
+            };
+            var result = _contentsServices.UpdateContent(content);
+            if (result)
+                return RedirectToAction("Index");
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult DeleteText(int ID)
+        {
+            var result = _contentsServices.DeleteContent(ID);
+            if (result)
+                return RedirectToAction("Index");
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult AddText(string title, string text)
+        {
+            var result = _contentsServices.AddText(title, text, 2);
+            if (result)
+                return RedirectToAction("Index");
+            return RedirectToAction("Index");
         }
     }
 }
--- Dilek/Areas/Admin/Controllers/KastamonuFanilasiController.cs
+++ Dilek/Areas/Admin/Controllers/KastamonuFanilasiController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Domains;
+using Services.ContentsServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,51 @@ namespace Dilek.Areas.Admin.Controllers
 {
     public class KastamonuFanilasiController : BaseController
     {
-        // GET: Admin/KastamonuFanilasi
+        public readonly IContentsServices _contentsServices;
+        public KastamonuFanilasiController()
+        {
+        }
+        public KastamonuFanilasiController(
+            IContentsServices contentsServices)
+        {
+            _contentsServices = contentsServices;
+        }
         public ActionResult Index()
         {
-            return View();
+            var result = _contentsServices.getAllContentsByDBCode(3);
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult UpdateText(int ID, string title, string text, DateTime CreatedDate, int DBCode)
+        {
+            Contents content = new Contents()
+            {
+                CreatedDate = CreatedDate,
+                DBCode = DBCode,
+                ID = ID,
+                Text = text,
+                Title = title
+            };
+            var result = _contentsServices.UpdateContent(content);
+            if (result)
+                return RedirectToAction("Index");
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult DeleteText(int ID)
+        {
+            var result = _contentsServices.DeleteContent(ID);
+            if (result)
+                return RedirectToAction("Index");
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult AddText(string title, string text)
+        {
+            var result = _contentsServices.AddText(title, text, 3);
+            if (result)
+                return RedirectToAction("Index");
+            return RedirectToAction("Index");
         }
     }
 }
